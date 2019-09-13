ad5218049cf5eb59c15e4e7112fdad9822525c18
takumi <tako.13.momi@gmail.com>
2019-09-13 12:31:48 +0900

    Capistrano-2

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,7 +3,7 @@
 lock '3.11.1'
 
 # Capistranoのログの表示に利用する
-set :application, '自身のアプリケーション名'
+set :application, 'chat-space'
 
 # どのリポジトリからアプリをpullするかを指定する
 set :repo_url,  'git@github.com:2019takumi/chat-space.git'
